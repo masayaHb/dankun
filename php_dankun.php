36111cd002c53aeceb9b8c4b7d61a5ba9b66f468
masayaHb <hm2kk9586@icloud.com>
2020-10-18 19:03:52 +0900

    Gitの練習

---
+++ php_dankun.php
@@ -0,0 +1,11 @@
+<?php
+$data = ['おはようございます','こんにちは','こんばんは'];
+date_default_timezone_set('Asia/Tokyo');
+$t = date("G");
+if($t > 6 and $t < 12):
+    print $data[0];
+elseif($t >= 12 and $t < 18):
+    print $data[1];
+else:
+    print $data[2];
+endif;
